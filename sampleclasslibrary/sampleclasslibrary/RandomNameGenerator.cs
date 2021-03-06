1cadfcaf26e4d756bdeeafaf89b4068f3ea5c236
Vincent van der Walt <vincent.vanderwalt@gmail.com>
2021-03-06 23:13:45 +0000

    initial sample library

---
+++ sampleclasslibrary/sampleclasslibrary/RandomNameGenerator.cs
@@ -0,0 +1,14 @@
+﻿using RandomDataGenerator.FieldOptions;
+using RandomDataGenerator.Randomizers;
+
+namespace sampleclasslibrary
+{
+    public static class RandomNameGenerator
+    {
+        public static string GetRandomName()
+        {
+            var randomFullName = RandomizerFactory.GetRandomizer(new FieldOptionsFullName());
+            return randomFullName.Generate();
+        }
+    }
+}
